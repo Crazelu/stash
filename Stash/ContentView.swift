a2efee4047bc4b88be74ac004c044458c1fbb48b
Crazelu <eberelucky64@gmail.com>
2024-02-25 08:56:58 +0100

    Initial Commit

---
+++ Stash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stash
+//
+//  Created by LUCKY EBERE on 25/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stash/StashApp.swift
@@ -0,0 +1,17 @@
+//
+//  StashApp.swift
+//  Stash
+//
+//  Created by LUCKY EBERE on 25/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct StashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
